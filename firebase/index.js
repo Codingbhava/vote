27f38c59fce08b036c0aea8b93959eefde3a9209
OMKAR SINGH <91762625+Codingbhava@users.noreply.github.com>
2024-04-12 20:41:08 +0530

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyBThipAcJqwPh5ABYPRQQQjLQp-eT86HYY",
-  authDomain: "omkar--singh.firebaseapp.com",
-  projectId: "omkar--singh",
-  storageBucket: "omkar--singh.appspot.com",
-  messagingSenderId: "30549289261",
-  appId: "1:30549289261:web:bdc2823a8ff77397eb0ba4",
-  measurementId: "G-ERYT13E12C"
+  apiKey: "add your apikey",
+  authDomain: "add your firebase config",
+  projectId: "add your firebase config",
+  storageBucket: "add your firebase config",
+  messagingSenderId: "add your firebase config",
+  appId: "add your firebase config",
+  measurementId: "add your firebase config"
 };
 const app = initializeApp(firebaseConfig);
 export const AUTH = getAuth(app);
